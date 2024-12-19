13d5ac591856d198c764ac4420c2272218ee4f35
OmMoholkar <omoholkar30@gmail.com>
2024-12-20 03:56:30 +0530

    added data class and lazy column

--- app/src/main/java/com/example/shoppinglist/ShoppingList.kt
+++ app/src/main/java/com/example/shoppinglist/ShoppingList.kt
@@ -1,9 +1,5 @@
 package com.example.shoppinglist
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,32 +7,42 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.shoppinglist.ui.theme.ShoppingListTheme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            ShoppingListTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    ShoppingListApp()
-                }
+data class ShoppingListItems(
+    val id: Int,
+    var name: String,
+    var quantity: Int,
+    var isEditing: Boolean = false
+)
+
+@Composable
+fun ShoppingListApp(){
+    var sItems by remember { mutableStateOf(listOf<ShoppingListItems>()) }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(
+            onClick = { },
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        ) {
+            Text("Add Item")
+        }
+        LazyColumn(
+            modifier = Modifier.fillMaxSize().padding(16.dp)
+        ) {
+            items(sItems){
+
             }
         }
     }
 }
-
-

a3629c81107e0540f4ecb6a83566c42f65a743ed
OmMoholkar <omoholkar30@gmail.com>
2024-12-20 04:27:24 +0530

    added buttons and text fields to dialogue box

--- app/src/main/java/com/example/shoppinglist/ShoppingList.kt
+++ app/src/main/java/com/example/shoppinglist/ShoppingList.kt
@@ -4,13 +4,16 @@ package com.example.shoppinglist
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -33,6 +36,8 @@ fun ShoppingListApp(){
 
     var sItems by remember { mutableStateOf(listOf<ShoppingListItems>()) }
     var showDialogue by remember { mutableStateOf(false) }
+    var itemName by remember { mutableStateOf("") }
+    var itemQuantity by remember { mutableStateOf("") }
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -54,12 +59,41 @@ fun ShoppingListApp(){
     }
 
     if (showDialogue){
-        AlertDialog(onDismissRequest = {showDialogue = false}){
-            Text("hello")
-        }
-//        AlertDialog(onDismissRequest = {showDialogue = false} ){
-//            Text("hello")
-//        }
+        AlertDialog(
+            onDismissRequest = { showDialogue = false },
+            confirmButton = {
+                Row(
+                    modifier = Modifier.fillMaxWidth().padding(8.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Button(onClick = {showDialogue = false}) {
+                        Text("Cancel")
+                    }
+                    Button(onClick = {}) {
+                        Text("Add")
+                    }
+                }
+            },
+            title = {Text("Add Shopping Item")},
+            text = {
+                Column {
+                    OutlinedTextField(
+                        value = itemName,
+                        onValueChange = {itemName = it},
+                        singleLine = true,
+                        modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp),
+                        label = {Text("Item Name")}
+                    )
+                    OutlinedTextField(
+                        value = itemQuantity,
+                        onValueChange = {itemQuantity = it},
+                        singleLine = true,
+                        modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp),
+                        label = {Text("Quantity")}
+                    )
+                }
+            }
+        )
     }
 }
 
